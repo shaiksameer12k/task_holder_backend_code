517990f0701a7ef5d0844a2164042d36ab0c5f59
sameer.s@dcoretech.in <sameer.s@dcoretech.in>
2025-01-28 20:51:46 +0530

    updated org code

--- index.js
+++ index.js
@@ -19,45 +19,16 @@ app.use(
   })
 );
 
-// DBConnection();
+DBConnection();
 
 // routes start point
 
 app.use("/user", userRouter);
 
-// app.get("/", (req, res) => {
-//   return res.send("Hello task holder api !");
-// });
-
 app.get("/", (req, res) => {
-  // res.send("C://Users//ManoMarappan//Pictures");
-
-  // let isFileExist = fs.existsSync("C://Users//ManoMarappan//Pictures");
-  let isFileExist = fs.existsSync("C://Users//Shaiksameer//Videos//edited");
-
-  if (!isFileExist) {
-    console.log("C://Users//Shaiksameer//Videos//edited");
-    let filesList = fs.readdirSync("C://Users//Shaiksameer//Videos//edited");
-
-    return res.send(filesList);
-
-    // filesList.map((file) => {
-    //   fs.readFile(
-    //     `C://Users//Shaiksameer//Pictures//Screenshots//${file}`,
-    //     (err, data) => {
-    //       fs.writeFile(`../dummy_${file}`, data, (err) => {
-    //         if (err) {
-    //           console.error("Error writing file:", err);
-    //         } else {
-    //           return res.send(`File saved as output.png : ${file} `);
-    //         }
-    //       });
-    //     }
-    //   );
-    // });
-  } else {
-    return res.send("not exist");
-  }
+  return res.send("Hello task holder api !");
 });
+
+
 
 module.exports = app;
